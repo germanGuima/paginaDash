c5ccd31f705f178eeffc8dfa793ab02f0f7dbcd3
ger <german.guimaraynz@gmail.com>
2023-09-19 16:35:26 -0300

    callback uptade multi graphs

--- gdash2.py
+++ gdash2.py
@@ -23,26 +23,26 @@ import sensores_callbacks
 
 
 
-def get_data_files_names():
+# def get_data_files_names():
 
-    dataList = glob('data/*.json')
-    names = [d.split('/')[-1].split('.')[0]
-                    for d in dataList]
-    return dataList,names
+#     dataList = glob('data/*.json')
+#     names = [d.split('\\')[-1].split('.')[0]
+#                     for d in dataList]
+#     return dataList,names
 
-# def openJsonAsDf(Jdata):
-#     with open(str(Jdata),'r') as json_file:
-#      Jdata3 = json.load(json_file)
-#     return pd.DataFrame(Jdata3)
-
-dl,names=get_data_files_names()
-namesS = [str(elemento) for elemento in names]
-# df = pd.DataFrame()
+# dl,names=get_data_files_names()
+# namesS = [str(elemento) for elemento in names]
 
 
 app.layout = html.Div(
 [
-
+    dcc.Interval(
+        id='intervalo-actualizacion',
+        interval=20*1000,  # Intervalo en milisegundos (en este caso, cada segundo)
+        n_intervals=0  # Valor inicial de n_intervals
+    ),
+    
+    dcc.Store(id='storeJ'),
      # ---------------------Imagen-------------------------------
     html.Img(src='https://ddhh.unq.edu.ar/wp-content/uploads/2017/04/Logo-UNQ-RGB.png', alt='Imagen de ejemplo',style={'width': '25%', 'height': 'auto'}),
 # --------------------Titulo--------------------------------
@@ -52,63 +52,17 @@ app.layout = html.Div(
 
  #-------------------Selector de Sensores-Variables-------------------
         
-        dcc.Store(id='data-store'),
-        dcc.Dropdown( id='dropSensor',
-                     options=[{'label': namesS, 'value': dl} for namesS, dl in zip(namesS, dl)],multi=True),
-        dcc.Dropdown( id='dropVar',options=[],multi=True,style={"width": 800}),
+    dcc.Store(id='data-store'),
+    dcc.Dropdown( id='dropSensor',options=[],multi=True),
+    dcc.Dropdown( id='dropVar'   ,options=[],multi=True,style={"width": 800}),
      # --------------------------Registro--------------------------
-        html.H1("Registro Histórico", style={'color': 'white','text-align': 'center'}),
-        dcc.Graph(id='my-graph')
+    html.H1("Registro Histórico", style={'color': 'white','text-align': 'center'}),
+        #dcc.Graph(id='my-graph')
+    html.Div(id='figures-container')
 
 ] ,style={'backgroundColor': '#411023', 'padding': '50px'})
 
-# from callbacks import * 
-# #--------------------Elegir Sensor-----------------------
-# @app.callback(
-#     Output('data-store','data'),
-#     Input('dropSensor','value'),
-# )
-# def update_output(Valor):
-#     if Valor!=None:
-#         df=openJsonAsDf(Valor)
-#         return df.to_dict()
-#     else:
-#         no_update
 
-#     #--------------------Elegir variable-----------------------
-# @app.callback(
-#     Output('dropVar', 'options') ,  # Actualizar las opciones del dropdown
-#     Input('data-store','data')
-# )
-# def actualizar_opciones(datos):
-#     if datos:
-
-#         df3=pd.DataFrame(datos)
-#         del df3['tiempo']
-#         opciones=[{'label': col , 'value':col} for col in df3.columns]
-#     else:
-#         opciones=[]
-#     return opciones
-# #------------------grafico  -----------------
-# @app.callback(
-#     Output('my-graph', 'figure' ),
-#     [Input('dropVar','value'),
-#     Input('data-store','data')],
-#     prevent_initial_call=True
-# )
-# def update_graph(selected_column,datos):
-  
-#     df3=pd.DataFrame(datos)
-#     if selected_column is None:
-#         return no_update
-#     if any(column not in df3.columns for column in selected_column):
-#         #filtro seleccionadas.
-#         selected=[column for column in selected_column if column in df3.columns]
-#         fig=px.line(df3, x='tiempo', y=selected, title='Valores Medidos')
-#         return fig
-#     else:
-#         fig=px.line(df3, x='tiempo', y=selected_column, title='Valores Medidos')
-#         return fig 
 
 
 if __name__ == '__main__':
--- sensores_callbacks.py
+++ sensores_callbacks.py
@@ -1,18 +1,46 @@
-import dash
+
 from dash import Output, Input, State, no_update,callback
 import pandas as pd
 import plotly.express as px
-import json 
+import json
 from plotly import graph_objects as go
-
+from dash import dcc
+from dash import Patch
 #from gdash2 import app
+from glob import glob
 
 def openJsonAsDf(Jdata):
     with open(str(Jdata),'r') as json_file:
      Jdata3 = json.load(json_file)
     return pd.DataFrame(Jdata3)
-  
+
+
+def get_data_files_names():
+
+    dataList = glob('data/*.json')
+    names = [d.split('/')[-1].split('.')[0]
+                    for d in dataList]
+    return dataList,names
+
+
+#-------------------- levantar datos-----------------------
+@callback(  
+          Output('dropSensor','options'),
+        Input('intervalo-actualizacion','n_intervals'),
+)
+def uptade_names(n_intervals):
+    dl,names=get_data_files_names()
+    namesS = [str(elemento) for elemento in names]
+    op={'dl': dl, 'names': names, 'nombre': namesS}
+
+    opciones=[ {'label': namesS, 'value': dl} for namesS, dl in zip(op['nombre'], op['dl']) ]  
+    #print(opciones)    
+    return opciones
+
+
 #--------------------Elegir Sensor-----------------------
+
+
 @callback(
     Output('data-store','data'),
     Input('dropSensor','value'),
@@ -27,11 +55,10 @@ def update_output(valor):
         #print(elemento)
         df =openJsonAsDf(elemento)
         name = elemento.split('/')[-1].split('.')[0]
-        #print(df)
         dic[name]=df.to_dict()
-    #print(dic)
     return dic# retorno diccionario
- 
+    
+
     #--------------------Elegir variable-----------------------
 @callback(
     Output('dropVar', 'options') ,  # Actualizar las opciones del dropdown
@@ -48,49 +75,120 @@ def actualizar_opciones(datos):
         opciones=[{'label': col , 'value':col} for col in l0]
     else:
         opciones=[]
-    return opciones
+    return opciones 
 
 #------------------grafico  -----------------
 @callback(
-    Output('my-graph', 'figure' ),
+    Output('figures-container', 'children' ),
     Input('dropVar','value'),
-    Input('data-store','data'),
-    State('my-graph', 'figure' ),
+    State('data-store','data'),
+    #State('figures-container', 'children' ),
     prevent_initial_call=True
 )
+# crea graficos si no hay variables
+def create_graph(selected_column,datos):#,fig):
 
-#esta funcion hay que corregir
-def update_graph(selected_column,datos,fig):
-    if selected_column is None:
-        return no_update
-    if fig is None:
+    figs=[]
+    for sel in selected_column:
         fig = go.Figure()
-    else:
-        fig = go.Figure(**fig)
-        fig.data =[]
-    #print(fig)
-    
-    for datos_key,datos_val in datos.items():
-        df = pd.DataFrame(datos_val)
-        for col_sel in selected_column:
-            if col_sel in df.columns:
-                xx= 'tiempo'
-                yy= col_sel
-                
 
-                this_line = go.Scatter(x=df[xx],
-                                       y=df[yy])
-                #agregar legend correspondiente
+        for datos_key,datos_val in datos.items():
+            df = pd.DataFrame(datos_val)
+            if sel in df.columns:
+                xx= 'tiempo'
+                yy= sel
+                this_line = go.Scatter(
+                        x=df[xx],
+                        y=df[yy],
+                        name=f'{datos_key}'         
+                )
                 fig.add_trace(this_line)
-    return fig
-    # df3=pd.DataFrame(datos)
-    # if selected_column is None:
-    #     return no_update
-    # if any(column not in df3.columns for column in selected_column):
-    #     #filtro seleccionadas.
-    #     selected=[column for column in selected_column if column in df3.columns]
-    #     fig=px.line(df3, x='tiempo', y=selected, title='Valores Medidos')
-    #     return fig
-    # else:
-    #     fig=px.line(df3, x='tiempo', y=selected_column, title='Valores Medidos')
-    #     return fig 
+                fig.update_layout(title=f'{yy}')
+        g=dcc.Graph(figure=fig ,id={'type':'graph',
+                                  'id':sel})
+        figs.append(g)
+    return figs
+
+
+from dash import MATCH,ALL, callback_context
+
+@callback(Output({'type':'graph','id':MATCH},'figure'),
+          Input('data-store','data'),
+          State({'type':'graph','id':MATCH},'figure'),
+          prevent_initial_callback=True
+)
+#actualiza graficos si ya existen
+def update(data,fig):
+    print("")
+    print("")
+    print("cb context")
+    print(len(callback_context.states_list))
+    print(callback_context.states_list[0]['id']['id'])
+    return no_update#fig
+
+
+# #------------------grafico  -----------------
+# @callback(
+#     Output('figures-container', 'children' ),
+#     Input('dropVar','value'),
+#     Input('data-store','data'),
+#     State('figures-container', 'children' ),
+#     prevent_initial_call=True
+# )
+
+# #esta funcion hay que corregir
+# def update_graph(selected_column,datos,fig):
+#     figuras = {}
+#     this_line= {}
+#     lines={}
+
+#     if selected_column is None:
+#         return no_update
+#     if fig is None:
+#         fig = go.Figure()
+#     for datos_key,datos_val in datos.items():
+#         df = pd.DataFrame(datos_val)
+#         for col_sel in selected_column:
+#             if col_sel in df.columns:
+#                 xx= 'tiempo'
+#                 yy= col_sel
+
+#                      #no existe variable
+#                 if yy not in figuras:
+
+#                     fig=go.Figure()
+
+#                     this_line[yy] = go.Scatter(
+#                         x=df[xx],
+#                         y=df[yy],
+#                         name=f'{datos_key}'         
+#                     )
+              
+#                     fig=go.Figure()
+#                     fig.add_trace(this_line[yy])
+#                     fig.update_layout(title=f'{yy}')
+#                     lines[datos_key]=this_line[yy]
+#                     figuras[yy]=(dcc.Graph(figure=fig))
+#                     #
+
+
+#                 else:    #ya existe variable
+#                     fig=go.Figure()
+#                     for item in lines:
+#                         linea=lines[item]
+#                         fig.add_trace(linea)
+
+#                     this_line[yy] = go.Scatter(x=df[xx],
+#                             y=df[yy],
+#                             name=f'{yy} - {datos_key}' 
+#                     )   
+
+#                     fig.add_trace(this_line[yy])
+#                     figuras[yy] =(dcc.Graph(figure=fig))  
+#                     fig.update_layout(title=f'{yy}')
+   
+
+#     return  [ figu[1]    for figu in list(figuras.items())  ] # dict to list
+
+
+
